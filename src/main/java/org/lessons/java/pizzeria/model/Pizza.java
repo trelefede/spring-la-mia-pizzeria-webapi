076a9382a810c67bbc3de509dbb239cd4ac6f046
TreleaniFederico <treleani.federico@gmail.com>
2023-02-14 15:20:40 +0100

    add model Pizza

---
+++ src/main/java/org/lessons/java/pizzeria/model/Pizza.java
@@ -0,0 +1,62 @@
+package org.lessons.java.pizzeria.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pizze")
+public class Pizza {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String name;
+	private String description;
+	private BigDecimal price;
+
+	@Column(name = "image", nullable = true)
+	private String imgUrl;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+}
