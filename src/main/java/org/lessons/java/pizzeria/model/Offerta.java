e8317c8aeaa5e6a4b32bf919d76df102ef390d4a
TreleaniFederico <treleani.federico@gmail.com>
2023-02-22 15:50:54 +0100

    add Offerta model content

--- src/main/java/org/lessons/java/pizzeria/model/Offerta.java
+++ src/main/java/org/lessons/java/pizzeria/model/Offerta.java
@@ -1,5 +1,65 @@
 package org.lessons.java.pizzeria.model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+
 public class Offerta {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idInteger;
+
+	@NotNull
+	private String titolo;
+	private LocalDate inizio;
+	private LocalDate fine;
+
+	@ManyToOne
+	private Pizza pizza;
+
+	public Integer getIdInteger() {
+		return idInteger;
+	}
+
+	public void setIdInteger(Integer idInteger) {
+		this.idInteger = idInteger;
+	}
+
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public LocalDate getInizio() {
+		return inizio;
+	}
+
+	public void setInizio(LocalDate inizio) {
+		this.inizio = inizio;
+	}
+
+	public LocalDate getFine() {
+		return fine;
+	}
+
+	public void setFine(LocalDate fine) {
+		this.fine = fine;
+	}
+
+	public Pizza getPizza() {
+		return pizza;
+	}
+
+	public void setPizza(Pizza pizza) {
+		this.pizza = pizza;
+	}
 
 }
