c76e52240947e401950537fb1b824af58061527e
TreleaniFederico <treleani.federico@gmail.com>
2023-02-22 16:35:08 +0100

    created table in db

--- src/main/java/org/lessons/java/pizzeria/controller/OffertaController.java
+++ src/main/java/org/lessons/java/pizzeria/controller/OffertaController.java
@@ -1,5 +1,32 @@
 package org.lessons.java.pizzeria.controller;
 
+import org.lessons.java.pizzeria.model.Offerta;
+import org.lessons.java.pizzeria.model.Pizza;
+import org.lessons.java.pizzeria.repository.PizzaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/offerte")
 public class OffertaController {
 
+	@Autowired
+	PizzaRepository pizzaRepository;
+
+	@GetMapping("/create")
+	public String create(@RequestParam(name = "pizzaId", required = true) Integer pizzaId, Model model) {
+
+		Offerta offerta = new Offerta();
+
+		Pizza pizza = pizzaRepository.getReferenceById(pizzaId);
+		offerta.setPizza(pizza);
+
+		model.addAttribute("offerta", offerta);
+
+		return ("offerte/createOfferte");
+	}
 }
--- src/main/java/org/lessons/java/pizzeria/model/Offerta.java
+++ src/main/java/org/lessons/java/pizzeria/model/Offerta.java
@@ -2,32 +2,42 @@ package org.lessons.java.pizzeria.model;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
+@Entity
 public class Offerta {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer idInteger;
+	private Integer id;
 
-	@NotNull
+	@NotNull(message = "Titolo non può essere nullo")
+	@NotEmpty(message = "Titolo non può essere vuoto")
 	private String titolo;
+
+	@NotNull(message = "Data inizio offerta non può essere nullo")
+	@NotEmpty(message = "Data inizio offerta non può essere vuoto")
 	private LocalDate inizio;
+
+	@NotNull(message = "Data fine offerta non può essere nullo")
+	@NotEmpty(message = "Data fine offerta non può essere vuoto")
 	private LocalDate fine;
 
 	@ManyToOne
 	private Pizza pizza;
 
 	public Integer getIdInteger() {
-		return idInteger;
+		return id;
 	}
 
 	public void setIdInteger(Integer idInteger) {
-		this.idInteger = idInteger;
+		this.id = idInteger;
 	}
 
 	public String getTitolo() {
