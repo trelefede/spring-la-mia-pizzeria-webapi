23e332b03eda880d3a8dada5dc0d7720eda8958f
TreleaniFederico <treleani.federico@gmail.com>
2023-02-24 17:47:54 +0100

    add update and delete function

--- src/main/java/org/lessons/java/pizzeria/controller/api/IndexApiController.java
+++ src/main/java/org/lessons/java/pizzeria/controller/api/IndexApiController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,9 +45,14 @@ public class IndexApiController {
 		return pizzaRepo.save(pizza);
 	}
 
-	@PutMapping("{id}")
+	@PutMapping("edit/{id}")
 	public Pizza update(@RequestBody Pizza pizza, @PathVariable("id") Integer id) {
-		Pizza pizzaSelected = pizzaRepo.getReferenceById(id);
+		pizza.setId(id);
 		return pizzaRepo.save(pizza);
+	}
+
+	@DeleteMapping("{id}")
+	public void delete(@PathVariable("id") Integer id) {
+		pizzaRepo.deleteById(id);
 	}
 }
